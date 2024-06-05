fd31b6fa709cb720a35c143bfccf6b0f6959b6dd
Yulia Grinenko <yuliaazarova1@gmail.com>
2024-06-05 15:31:52 +0100

    link style changed

--- src/Weather.js
+++ src/Weather.js
@@ -39,6 +39,37 @@ export default function Weather() {
             </p>
           </div>
         </div>
+        <footer>
+          <p>
+            This project was coded by {}
+            <a
+              href="https://github.com/YuliaGrinenko"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              Yulia Hrinenko
+            </a>
+            {} and is {}
+            <a
+              href="https://github.com/YuliaGrinenko/weather-react"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              on GitHub
+            </a>
+            {} and {}
+            <a
+              href="https://weather-app-react-yulia.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              hosted on Netlify
+            </a>
+          </p>
+        </footer>
       </div>
     </div>
   );
