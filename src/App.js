dc00cdff1b161196a8806ce8a92d37eaca9ff863
Yulia Grinenko <yuliaazarova1@gmail.com>
2024-06-15 16:27:42 +0100

    footer moved to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <Weather defaultCity="New York" />
+      <footer>
+        <p>
+          This project was coded by {}
+          <a
+            href="https://github.com/YuliaGrinenko"
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+          >
+            Yulia Hrinenko
+          </a>
+          {} and is {}
+          <a
+            href="https://github.com/YuliaGrinenko/weather-react"
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+          >
+            on GitHub
+          </a>
+          {} and {}
+          <a
+            href="https://weather-app-react-yulia.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+          >
+            hosted on Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
-
-export default App;
