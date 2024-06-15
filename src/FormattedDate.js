007b7bd602e912ef5e8c46a24c11a67541321734
Yulia Grinenko <yuliaazarova1@gmail.com>
2024-06-15 17:56:10 +0100

    formatted date added

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,6 +1,27 @@
 import React from "react";
 
-export default function FormattedDate() {
-    let hours = getHours(date);
-  return <div>day, hours:minutes</div>;
+export default function FormattedDate(props) {
+  let hours = props.date.getHours();
+  if (hours <= 0) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.date.getMinutes();
+  if (minutes <= 0) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[props.date.getDay()];
+  return (
+    <div>
+      {day}, {hours}:{minutes}
+    </div>
+  );
 }
