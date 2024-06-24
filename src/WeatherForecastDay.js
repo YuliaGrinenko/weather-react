39729bfdd3fd4b283ea01c38e93308e5a548ff92
Yulia Grinenko <yuliaazarova1@gmail.com>
2024-06-24 16:06:11 +0100

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
   return (
     <div>
-      <div>{props.data.dt * 1000}</div>
-      <div>{props.data.temp.max}</div>
-      <div>{props.data.temp.min}</div>
+      <div> {props.data.dt * 1000}</div>
+      <div>{maxTemperature()}</div>
+
+      <div>{minTemperature()}</div>
     </div>
   );
 }
