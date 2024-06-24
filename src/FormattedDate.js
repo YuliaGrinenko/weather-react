18b2c7507bedb808ccaed9fb4e7d13210e81e12b
Yulia Grinenko <yuliaazarova1@gmail.com>
2024-06-24 15:08:13 +0100

    code for showing hours and minutes edited

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function FormattedDate(props) {
   let hours = props.date.getHours();
-  if (hours <= 0) {
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   let minutes = props.date.getMinutes();
-  if (minutes <= 0) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
   let days = [
@@ -20,7 +20,6 @@ export default function FormattedDate(props) {
   ];
   let day = days[props.date.getDay()];
   return (
-    
     <div>
       {day}, {hours}:{minutes}
     </div>
