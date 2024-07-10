72af9eaeaa3e60f241491d4d740a84fec9c57919
Yulia Grinenko <yuliaazarova1@gmail.com>
2024-07-10 16:52:42 +0100

    Update Units.js

--- src/Units.js
+++ src/Units.js
@@ -17,7 +17,6 @@ export default function Units(props) {
   }
 
   function handleUnitClick(event) {
-    event.preventDefault();
     if (
       event.type === "click" ||
       (event.type === "touchend" && event.cancelable)
